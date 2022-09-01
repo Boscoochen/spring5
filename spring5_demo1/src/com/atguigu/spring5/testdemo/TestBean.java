2e855c8bf0c9f214910d0fac38c431ab9c685810
Boscoochen <boscochen203@gmail.com>
2022-09-01 14:26:20 -0500

    add 内部bean 和 级联赋值

--- spring5_demo1/src/com/atguigu/spring5/testdemo/TestBean.java
+++ spring5_demo1/src/com/atguigu/spring5/testdemo/TestBean.java
@@ -1,5 +1,6 @@
 package com.atguigu.spring5.testdemo;
 
+import com.atguigu.spring5.bean.Emp;
 import com.atguigu.spring5.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -7,12 +8,32 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestBean {
     @Test
-    public void testBean() {
+    public void testBean1() {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("bean2.xml");
 
         UserService userService = context.getBean("userService", UserService.class);
 
         userService.add();
+    }
+
+    @Test
+    public void testBean2() {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean3.xml");
+
+        Emp emp = context.getBean("emp", Emp.class);
+
+        emp.add();
+    }
+
+    @Test
+    public void testBean3() {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean4.xml");
+
+        Emp emp = context.getBean("emp", Emp.class);
+
+        emp.add();
     }
 }
