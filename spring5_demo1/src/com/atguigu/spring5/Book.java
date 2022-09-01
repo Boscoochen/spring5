8ba3d5034a584474457c8d5a34b83747eaaba43b
Boscoochen <boscochen203@gmail.com>
2022-09-01 13:01:45 -0500

    add CDAT转义特殊符号

--- spring5_demo1/src/com/atguigu/spring5/Book.java
+++ spring5_demo1/src/com/atguigu/spring5/Book.java
@@ -3,6 +3,7 @@ package com.atguigu.spring5;
 public class Book {
     private String bname;
     private String bauthor;
+    private String address;
 
     public void setBname(String bname) {
         this.bname = bname;
@@ -12,7 +13,11 @@ public class Book {
         this.bauthor = bauthor;
     }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public void testDemo() {
-        System.out.println(bname + "::" + bauthor);
+        System.out.println(bname + "::" + bauthor + "::" + address);
     }
 }
--- spring5_demo1/src/com/atguigu/spring5/Orders.java
+++ spring5_demo1/src/com/atguigu/spring5/Orders.java
@@ -4,6 +4,7 @@ public class Orders {
     private String oname;
     private String address;
 
+
     public Orders(String oname, String address) {
         this.oname = oname;
         this.address = address;
