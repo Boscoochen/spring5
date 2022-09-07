2adf455de072ae8bef1a71dfc013d4cbcd5424aa
Boscoochen <boscochen203@gmail.com>
2022-09-07 13:13:54 -0500

    xml配置文件事务回滚

--- spring5_demo1/src/com/atguigu/spring5/test事务/TestAccount.java
+++ spring5_demo1/src/com/atguigu/spring5/test事务/TestAccount.java
@@ -9,9 +9,17 @@ import org.springframework.transaction.PlatformTransactionManager;
 public class TestAccount {
 
     @Test
-    public void testAccount() {
+    public void testAccount1() {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("bean15.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+        userService.accountMoney();
+    }
+
+    @Test
+    public void testAccount2() {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean16.xml");
         UserService userService = context.getBean("userService", UserService.class);
         userService.accountMoney();
     }
