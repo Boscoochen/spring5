57112ab15e9dd1f2cae5aa967ff7d9afb7a13976
Boscoochen <boscochen203@gmail.com>
2022-09-07 14:47:22 -0500

    添加Nullable注解和函数式注册对象

--- spring5_demo1/src/com/atguigu/spring5/test事务/TestAccount.java
+++ spring5_demo1/src/com/atguigu/spring5/test事务/TestAccount.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.transaction.PlatformTransactionManager;
 
 public class TestAccount {
@@ -35,5 +36,18 @@ public class TestAccount {
                 new AnnotationConfigApplicationContext(TxConfig.class);
         UserService userService = context.getBean("userService", UserService.class);
         userService.accountMoney();
+    }
+
+    //函数式风格创建对象，交给spring进行管理
+    @Test
+    public void testGenericApplicationContext() {
+        //1 创建GenericApplicationContext
+        GenericApplicationContext context = new GenericApplicationContext();
+        //2 调用context的方法对象注册
+        context.refresh();
+        context.registerBean("user1", User.class, ()-> new User());
+        //3 获取spring注册的对象
+        User user = (User)context.getBean("user1");
+        System.out.println(user);
     }
 }
---
+++ spring5_demo1/src/com/atguigu/spring5/test事务/User.java
@@ -0,0 +1,5 @@
+package com.atguigu.spring5.test事务;
+
+public class User {
+
+}
